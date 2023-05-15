3b6bd68c784d79f1217c0a6a14700bfe203d80ed
meharpalbasi <100152988+meharpalbasi@users.noreply.github.com>
2023-05-15 23:30:01 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,12 +13,12 @@ import pandas as pd
 
 st.title('Finance Dashboard')
 
-tickers = ('LGEN', 'MNG', 'MSFT', 'AFRM' , 'RIOT', 'AMD', 'CRWD', 'AAPL', 'GOOGL', 'RIO', 'GSK'  'BTC-USD', 'ETH-USD', 'LINK-USD', 'AVAX-USD' )
+tickers = ('LGEN', 'MNG', 'MSFT', 'AFRM' , 'RIOT', 'AMD', 'CRWD', 'AAPL', 'GOOGL', 'RIO', 'GSK',  'BTC-USD', 'ETH-USD', 'LINK-USD', 'AVAX-USD' )
 
 dropdown = st.multiselect('Pick your assets',
                           tickers)
 
-start = st.date_input('Start', value = pd.to_datetime('2021-5-01'))
+start = st.date_input('Start', value = pd.to_datetime('2015-01-01'))
 end = st.date_input('End', value = pd.to_datetime('today'))
 
 #the date part of the dropdown 
